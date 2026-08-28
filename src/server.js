7184ca211ee831182e9545c84364f374e22cba4f
tchandok30 <tanishachandok@gmail.com>
2026-08-28 22:42:28 +0530

    added routes controllers and services

--- src/server.js
+++ src/server.js
@@ -1,35 +1,29 @@
-const express=require("express")
+const express=require("express");
+
+const orderRoutes = require("./routes/orderRoutes");
+const vehicleRoutes = require("./routes/vehicleRoutes");
+const driverRoutes = require("./routes/driverRoutes");
 const app=express();
 const PORT = 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-}); 
+app.use(express.json());   
 app.get("/", (req, res) => {
     res.json({
     message: "RouteForge Backend"
 });
 });
-const orders = [
-    {
-        id: "ORD001",
-        weight: 300,
-        priority: "HIGH"
-    },
-    {
-        id: "ORD002",
-        weight: 500,
-        priority: "NORMAL"
-    }
-];
-app.get("/api/orders", (req, res) => {
-    res.json(orders);
-});
-app.post("/api/orders", (req, res) => {
+app.use("/api/orders", orderRoutes);
+// GET /api/orders
+//        ↓
+// server.js
+//        ↓
+// orderRoutes.js
+//        ↓
+// router.get("/")
+//        ↓
+// response
+app.use("/api/vehicles", vehicleRoutes);
 
-    const order = req.body;
-
-    orders.push(order);
-
-    res.json(order);
-
-});
+app.use("/api/drivers", driverRoutes);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+}); 
